3f785e5ea64e715592255940bd1962bca699b6a0
Emerson MX <emersonmx@gmail.com>
2011-10-29 16:48:47 -0300

    Using 4 spaces for indetation.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- src/mxadt/stack.c
+++ src/mxadt/stack.c
@@ -25,110 +25,110 @@
 typedef struct mxadt_stack_element mxadt_stack_element;
 struct mxadt_stack_element
 {
-  void* data;
-  mxadt_stack_element* next;
+    void* data;
+    mxadt_stack_element* next;
 };
 
 struct mxadt_stack
 {
-  unsigned int size;
-  mxadt_stack_element* top;
+    unsigned int size;
+    mxadt_stack_element* top;
 };
 
 mxadt_stack* mxadt_stack_initialize()
 {
-  mxadt_stack* stack = malloc(sizeof(mxadt_stack));
+    mxadt_stack* stack = malloc(sizeof(mxadt_stack));
 
-  if (stack)
-  {
-    stack->size = 0;
-    stack->top = NULL;
-  }
+    if (stack)
+    {
+        stack->size = 0;
+        stack->top = NULL;
+    }
 
-  return stack;
+    return stack;
 }
 
 void mxadt_stack_finalize(mxadt_stack* stack)
 {
-  if (stack)
-  {
-    while (!mxadt_stack_empty(stack))
+    if (stack)
     {
-      mxadt_stack_pop(stack);
-    }
+        while (!mxadt_stack_empty(stack))
+        {
+            mxadt_stack_pop(stack);
+        }
 
-    free(stack);
-  }
+        free(stack);
+    }
 }
 
 unsigned int mxadt_stack_size(mxadt_stack* stack)
 {
-  if (stack)
-  {
-    return stack->size;
-  }
+    if (stack)
+    {
+        return stack->size;
+    }
 
-  return 0;
+    return 0;
 }
 
 void* mxadt_stack_top(mxadt_stack* stack)
 {
-  void* ret_val = NULL;
+    void* ret_val = NULL;
 
-  if (stack)
-  {
-    if (stack->top)
+    if (stack)
     {
-      ret_val = stack->top->data;
+        if (stack->top)
+        {
+            ret_val = stack->top->data;
+        }
     }
-  }
 
-  return ret_val;
+    return ret_val;
 }
 
 int mxadt_stack_push(mxadt_stack* stack, void* data)
 {
-  if (stack)
-  {
-    mxadt_stack_element* top = stack->top;
-    mxadt_stack_element* new_element = malloc(sizeof(mxadt_stack_element));
-    if (new_element)
+    if (stack)
     {
-      new_element->data = data;
-      new_element->next = top;
-      stack->top = new_element;
-      stack->size++;
-      return 0;
+        mxadt_stack_element* top = stack->top;
+        mxadt_stack_element* new_element = malloc(sizeof(mxadt_stack_element));
+        if (new_element)
+        {
+            new_element->data = data;
+            new_element->next = top;
+            stack->top = new_element;
+            stack->size++;
+            return 0;
+        }
     }
-  }
 
-  return -1;
+    return -1;
 }
 
 int mxadt_stack_pop(mxadt_stack* stack)
 {
-  if (stack)
-  {
-    if (stack->top)
+    if (stack)
     {
-      mxadt_stack_element* top = stack->top;
-      stack->top = top->next;
-      free(top);
-      stack->size--;
-      return 0;
+        if (stack->top)
+        {
+            mxadt_stack_element* top = stack->top;
+            stack->top = top->next;
+            free(top);
+            stack->size--;
+            return 0;
+        }
     }
-  }
 
-  return -1;
+    return -1;
 }
 
 int mxadt_stack_empty(mxadt_stack* stack)
 {
-  if (stack)
-  {
-    return stack->top == NULL;
-  }
+    if (stack)
+    {
+        return stack->top == NULL;
+    }
 
-  return 0;
+    return 0;
 }
 
--- test/stack_test.c
+++ test/stack_test.c
@@ -4,45 +4,45 @@
 
 int main()
 {
-  mxadt_stack* s = mxadt_stack_initialize();
+    mxadt_stack* s = mxadt_stack_initialize();
 
-  printf("Push NULL Test is ");
-  if (mxadt_stack_push(NULL, 0))
-    printf("OK.\n");
-  else
-    printf("Fail.\n");
+    printf("Push NULL Test is ");
+    if (mxadt_stack_push(NULL, 0))
+        printf("OK.\n");
+    else
+        printf("Fail.\n");
 
-  printf("Pop NULL Test is ");
-  if (mxadt_stack_pop(NULL))
-    printf("OK.\n");
-  else
-    printf("Fail.\n");
+    printf("Pop NULL Test is ");
+    if (mxadt_stack_pop(NULL))
+        printf("OK.\n");
+    else
+        printf("Fail.\n");
 
-  printf("Pop empty Test is ");
-  if (mxadt_stack_pop(s))
-    printf("OK.\n");
-  else
-    printf("Fail.\n");
+    printf("Pop empty Test is ");
+    if (mxadt_stack_pop(s))
+        printf("OK.\n");
+    else
+        printf("Fail.\n");
 
-  mxadt_stack_push(s, (void*) 10);
-  mxadt_stack_push(s, (void*) 15);
-  mxadt_stack_push(s, (void*) 9);
-  mxadt_stack_push(s, (void*) 1);
-  mxadt_stack_pop(s);
-  mxadt_stack_push(s, (void*) 5);
-
-  printf("Stack size: %u\n", mxadt_stack_size(s));
-
-  while(!mxadt_stack_empty(s))
-  {
-    printf("%d ", (int) mxadt_stack_top(s));
+    mxadt_stack_push(s, (void*) 10);
+    mxadt_stack_push(s, (void*) 15);
+    mxadt_stack_push(s, (void*) 9);
+    mxadt_stack_push(s, (void*) 1);
     mxadt_stack_pop(s);
-  }
-  printf("\n");
+    mxadt_stack_push(s, (void*) 5);
 
-  printf("Stack size: %u\n", mxadt_stack_size(s));
+    printf("Stack size: %u\n", mxadt_stack_size(s));
 
-  mxadt_stack_finalize(s);
-  return 0;
+    while(!mxadt_stack_empty(s))
+    {
+        printf("%d ", (int) mxadt_stack_top(s));
+        mxadt_stack_pop(s);
+    }
+    printf("\n");
+
+    printf("Stack size: %u\n", mxadt_stack_size(s));
+
+    mxadt_stack_finalize(s);
+    return 0;
 }
 
